28dc07efa476beabb6b46628d4f0d029f5088eac
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-03-09 21:11:27 +0800

    configure http-cookie

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cors({ origin: '*' }));
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:5173', methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], credentials: true, exposedHeaders: ['Set-Cookie'] }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 
 app.post('/api/v1/login', async (req, res) => {
 
-    res.cookie('refresh', 'sample-refresh', { httpOnly: true, sameSite: true })
+    res.cookie('refresh', 'sample-refresh', { secure: true, sameSite: 'lax', httpOnly: true, path: '/' });
 
     return res.status(200).json({ status: 200, data: [], message: 'Success' })
 });
@@ -20,7 +22,7 @@ app.get('/api/sample/test', async (req, res) => {
 
     const cookie = req.cookies.refresh;
 
-    return res.json({ cookie })
+    return res.json({ cookie, cookie_headers: req.headers.cookie })
 })
 
 module.exports = app;
